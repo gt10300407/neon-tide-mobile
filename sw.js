b158a5ffc49ddda3bd0ad866069592aaa2d5c42d
gt10300407 <zhidaole407@gmail.com>
2026-07-22 18:46:09 +0900

    Update NEON TIDE mobile v3.4 Korean

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neon-tide-v3-3";
+const CACHE = "neon-tide-v3-4-ko";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
