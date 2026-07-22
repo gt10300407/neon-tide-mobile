50f875ac9b9fb17221f264b1463c7097fbb7ca33
gt10300407 <zhidaole407@gmail.com>
2026-07-22 20:25:45 +0900

    Use standard relative virtual pad in NEON TIDE v3.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neon-tide-v3-8-visible-pad";
+const CACHE = "neon-tide-v3-9-standard-pad";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
