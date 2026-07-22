10cc0f2356d5d9278af53964d2a2ed220cb0be40
gt10300407 <zhidaole407@gmail.com>
2026-07-22 19:51:52 +0900

    Fix boss damage and unlock movement in NEON TIDE v3.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neon-tide-v3-6-responsive-balance";
+const CACHE = "neon-tide-v3-7-free-move-boss-damage";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
