ba4a381e5d3b9fd8da684cd62f4d7ad837e13653
gt10300407 <zhidaole407@gmail.com>
2026-07-22 19:37:31 +0900

    Update NEON TIDE mobile v3.6 balance and controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neon-tide-v3-5-analog-primary";
+const CACHE = "neon-tide-v3-6-responsive-balance";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
